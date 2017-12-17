578c59c13a6e0a8a4768fee27fce89eb5a87fb22
epiqueras <epiquerass@gmail.com>
2017-12-17 12:29:56 -0800

    fix(form-generator): broken import

--- src/components/form-generator.jsx
+++ src/components/form-generator.jsx
@@ -1,6 +1,6 @@
 import createFormGenerator from '../utils/create-form-generator'
 import store from '../index'
-import { TextInput } from './text-input'
+import TextInput from './text-input'
 
 export default createFormGenerator(
   { text: TextInput, number: TextInput },
